c60038dc4ed445b4b0c8f8a0866ebd962f700d10
dudaholanda1975 <isadoraholanda1975@gmail.com>
2023-08-31 21:29:35 -0300

    aula01

---
+++ aula01/main.js
@@ -0,0 +1,13 @@
+console.log ('Hello');
+//console.log eu uso para mostrar uma mensagem ao usuario
+
+//alert exibe uma mensagem de alerta ao usuario 
+alert('Hello'); 
+
+//para declarar variavel no js podemos usar "var", "let", "const"
+const pi = 3.14; 
+
+//SEMPRE que usar "const" para declarar uma variavel, o valor dela nao pode ser alterado!!!
+const pi = 3.14 
+alert(pi); 
+
